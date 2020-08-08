52bb9dae0147e01adc7aaa85b70b9f880da898a9
Lambert Schulze <schulze.lambert@googlemail.com>
2020-08-08 17:38:07 +0200

    floating Form for New Claim creation

--- src/App.js
+++ src/App.js
@@ -36,7 +36,39 @@ function App() {
                 </tbody>
             </table>
             </div>
-          <button type="button" className="btn btn-primary">New Claim</button>
+            <button type="button" className="btn btn-primary" data-toggle="modal" data-target="#exampleModal">New Claim</button>
+
+            <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+              <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Create New Claim</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                      <span aria-hidden="true">&times;</span>
+                    </button>
+                  </div>
+                  <div class="modal-body">
+                    <div className="input-group mb-3">
+                      <div className="input-group-prepend">
+                        <span className="input-group-text">Liefery-ID</span>
+                      </div>
+                      <input type="text" class="form-control" placeholder="Liefery-ID" />
+                    </div>
+                    <div className="input-group mb-3">
+                      <div className="input-group-prepend">
+                        <span className="input-group-text">Corier Name</span>
+                      </div>
+                      <input type="text" class="form-control" placeholder="Corier Full Name" />
+                    </div>
+                  </div>
+                  <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Add</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+
           </div>
         </div>
       </div>
