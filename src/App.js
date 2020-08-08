564099c0273aebde89ea8c553af4fa4b303a6df7
Lambert Schulze <schulze.lambert@googlemail.com>
2020-08-08 18:44:51 +0200

    fixed className for class

--- src/App.js
+++ src/App.js
@@ -38,32 +38,32 @@ function App() {
             </div>
             <button type="button" className="btn btn-primary" data-toggle="modal" data-target="#exampleModal">New Claim</button>
 
-            <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-              <div class="modal-dialog" role="document">
-                <div class="modal-content">
-                  <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Create New Claim</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <div className="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+              <div className="modal-dialog" role="document">
+                <div className="modal-content">
+                  <div className="modal-header">
+                    <h5 className="modal-title" id="exampleModalLabel">Create New Claim</h5>
+                    <button type="button" className="close" data-dismiss="modal" aria-label="Close">
                       <span aria-hidden="true">&times;</span>
                     </button>
                   </div>
-                  <div class="modal-body">
+                  <div className="modal-body">
                     <div className="input-group mb-3">
                       <div className="input-group-prepend">
                         <span className="input-group-text">Liefery-ID</span>
                       </div>
-                      <input type="text" class="form-control" placeholder="Liefery-ID" />
+                      <input type="text" className="form-control" placeholder="Liefery-ID" />
                     </div>
                     <div className="input-group mb-3">
                       <div className="input-group-prepend">
                         <span className="input-group-text">Courier Name</span>
                       </div>
-                      <input type="text" class="form-control" placeholder="Corier Full Name" />
+                      <input type="text" className="form-control" placeholder="Corier Full Name" />
                     </div>
                   </div>
-                  <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary">Add</button>
+                  <div className="modal-footer">
+                    <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="button" className="btn btn-primary">Add</button>
                   </div>
                 </div>
               </div>
