90931a838ed3bc4eae458c8e7e64324c77d6658c
Lambert Schulze <schulze.lambert@googlemail.com>
2020-08-12 00:55:05 +0200

    added claim actions to claimsReducer

--- src/reducers/claimsReducer.js
+++ src/reducers/claimsReducer.js
@@ -1,3 +1,5 @@
+import * as actions from '../actions/claimsActions'
+
 export const initialState = {
     claims:     [],
     loading:    false,
@@ -6,6 +8,12 @@ export const initialState = {
 
 export default function claimsReducer(state = initialState, action) {
     switch (action.type) {
+        case actions.GET_CLAIMS:
+            return {...state, loading: true}
+        case actions.GET_CLAIMS_SUCCESS:
+            return {claims: action.payload, loading: false, hasErrors: false}
+        case actions.GET_CLAIMS_FAILURE:
+            return {...state, loading: false, hasErrors: true}
         default:
             return state
     }
